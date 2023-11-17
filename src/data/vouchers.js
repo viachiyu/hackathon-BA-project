f3486e5c74319c94104995ff02041247981eaeef
lkrusso <lararusso.uk@gmail.com>
2023-11-17 13:52:54 +0000

    feat: create users and vouchers data files

---
+++ src/data/vouchers.js
@@ -0,0 +1,6 @@
+const vouchers = [
+  { id: "icvwyLmT", value: 350, isUsed: false },
+  { id: "BfdotjMv", value: 1267, isUsed: true },
+  { id: "U9crjd48", value: 2340, isUsed: false },
+];
+export default vouchers;
